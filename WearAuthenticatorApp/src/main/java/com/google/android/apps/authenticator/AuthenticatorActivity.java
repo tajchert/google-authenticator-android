f8aa0c104b6c52c00b6b7c5b1954727bf7e4735a
Michal Tajchert <mtajchert@gmail.com>
2015-04-02 16:56:54 +0200

    Initial Wear commit.

---
+++ WearAuthenticatorApp/src/main/java/com/google/android/apps/authenticator/AuthenticatorActivity.java
@@ -0,0 +1,24 @@
+package com.google.android.apps.authenticator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class AuthenticatorActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticator);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
